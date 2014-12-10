eeda77f8730833e3fe82ebecc29c03e39f1a6dab
Wonmin Jung <wonmin82@gmail.com>
2014-12-10 22:52:42 +0900

    Add zkbd configurations for zsh

--- install-zsh-config.sh
+++ install-zsh-config.sh
@@ -2,6 +2,9 @@
 cp -f -v ./zsh/zshenv ~/.zshenv
 mkdir -p -v ~/.zsh
 cp -f -v ./zsh/zshrc ~/.zsh/.zshrc
+mkdir -p -v ~/.zsh/.zkbd
+cp -f -v ./zsh/zkbd/xterm-256color-pc-linux-gnu ~/.zsh/.zkbd/
+cp -f -v ./zsh/zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/
 if [[ $(cat /etc/passwd | grep "^$USER" | awk -F ':' '{ print $7 }') != '/bin/zsh' ]]; then
 	sudo chsh --shell /bin/zsh wonmin82
 fi
