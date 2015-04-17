2f430f79d37f8c12e4acb8ef2a8a112744ff9298
Hoyoung Jung <k502000@naver.com>
2015-04-18 08:40:35 +0900

    fix zsh-config for OSX

--- install-zsh-config.sh
+++ install-zsh-config.sh
@@ -8,5 +8,5 @@ cp -f -v ./zsh/zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/
 ln -sf ~/.zsh/.zkbd/xterm-256color-pc-linux-gnu ~/.zsh/.zkbd/xterm-256color-unknown-linux-gnu
 ln -sf ~/.zsh/.zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/screen-256color-unknown-linux-gnu
 if [[ $(cat /etc/passwd | grep "^$USER" | awk -F ':' '{ print $7 }') != '/bin/zsh' ]]; then
-	sudo chsh --shell /bin/zsh wonmin82
+	sudo chsh -s /bin/zsh ${USER}
 fi
