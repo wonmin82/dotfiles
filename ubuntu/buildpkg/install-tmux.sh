86c8e81d8360149c78f6fb486c637ed9b146b3e9
Wonmin Jung <wonmin82@gmail.com>
2016-11-28 22:26:27 +0900

    Rename variable, and use full reference name
    ubuntu/buildpkg/install_tmux.sh: rename variable, and use full reference name
    ubuntu/buildpkg/install_vim.sh: rename variable, and use full reference name

--- ubuntu/buildpkg/install-tmux.sh
+++ ubuntu/buildpkg/install-tmux.sh
@@ -4,14 +4,14 @@ set -e -x
 
 source ./build-env.sh
 
-branch="2.3"
+tag="2.3"
 
 mkdir -p ${build_dir}
 pushd ${build_dir}
 
-git clone https://github.com/tmux/tmux.git --no-checkout --depth 1 --single-branch -b ${branch}
+git clone https://github.com/tmux/tmux.git --no-checkout --depth 1 --single-branch -b ${tag}
 pushd tmux
-git checkout ${branch} -b build
+git checkout refs/tags/${tag} -b build
 ./autogen.sh
 ./configure --target=x86_64-linux-gnu --host=x86_64-linux-gnu --build=x86_64-linux-gnu --prefix=${install_prefix}
 make -j ${jobs}
--- ubuntu/buildpkg/install-vim.sh
+++ ubuntu/buildpkg/install-vim.sh
@@ -4,14 +4,14 @@ set -e -x
 
 source ./build-env.sh
 
-branch="v7.4.1689"
+tag="v7.4.1689"
 
 mkdir ${build_dir}
 pushd ${build_dir}
 
-git clone https://github.com/vim/vim.git --no-checkout --depth 1 --single-branch -b ${branch}
+git clone https://github.com/vim/vim.git --no-checkout --depth 1 --single-branch -b ${tag}
 pushd vim
-git checkout ${branch} -b build
+git checkout refs/tags/${tag} -b build
 ./configure --target=x86_64-linux-gnu --host=x86_64-linux-gnu --build=x86_64-linux-gnu --prefix=${install_prefix} --with-features=huge --enable-multibyte --enable-rubyinterp --enable-tclinterp --enable-luainterp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-perlinterp --enable-luainterp --enable-gui=no --enable-cscope --enable-hangulinput
 make -j ${jobs}
 make -j ${jobs} install
