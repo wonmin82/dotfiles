e08c9888436ee3e6b41508a5f792719a3e1d9e37
Wonmin Jung <wonmin82@gmail.com>
2016-12-10 20:45:22 +0900

    Add root privilege check

--- ubuntu/ubuntu-16.04-setup.sh
+++ ubuntu/ubuntu-16.04-setup.sh
@@ -455,6 +455,11 @@ post_process()
 
 main()
 {
+	if [[ $EUID -ne 0 ]]; then
+		echo "This script must be run as root." 1>&2
+		exit 1
+	fi
+
 	apply_apt_configurations
 	install_apt_prerequisites
 	add_repo
