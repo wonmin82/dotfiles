640c19231040b35a8257dde29669f754e6fdf20b
Wonmin Jung <wonmin82@gmail.com>
2016-12-29 17:53:14 +0900

    Improve zsh installation

--- install-zsh-config.sh
+++ install-zsh-config.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
+
 cp -f -v ./zsh/zshenv ~/.zshenv
 mkdir -p -v ~/.zsh
 cp -f -v ./zsh/zshrc ~/.zsh/.zshrc
@@ -7,6 +8,15 @@ cp -f -v ./zsh/zkbd/xterm-256color-pc-linux-gnu ~/.zsh/.zkbd/
 cp -f -v ./zsh/zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/
 ln -sf ~/.zsh/.zkbd/xterm-256color-pc-linux-gnu ~/.zsh/.zkbd/xterm-256color-unknown-linux-gnu
 ln -sf ~/.zsh/.zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/screen-256color-unknown-linux-gnu
-if [[ $(cat /etc/passwd | grep "^$USER" | awk -F ':' '{ print $7 }') != '/bin/zsh' ]]; then
-	sudo chsh -s /bin/zsh ${USER}
+
+local CURRENT_SHELL="/bin/zsh"
+
+if (( $+commands[dscl] )); then
+	CURRENT_SHELL=$(dscl localhost -read /Local/Default/Users/$(id -un) UserShell | cut -d ' ' -f 2)
+else
+	CURRENT_SHELL=$(cat /etc/passwd | grep "^$(id -un)" | cut -d ':' -f 7)
+fi
+
+if [[ ${CURRENT_SHELL} != '/bin/zsh' ]]; then
+	sudo chsh -s /bin/zsh $(id -un)
 fi
