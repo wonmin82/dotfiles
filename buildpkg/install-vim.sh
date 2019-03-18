540135fc25bc2e019b12cc7ea7c4c62ddead9819
Wonmin Jung <wonmin82@gmail.com>
2019-03-18 17:48:36 +0900

    Apply temporary fix for vim-manpager

--- buildpkg/install-vim.sh
+++ buildpkg/install-vim.sh
@@ -4,7 +4,11 @@ set -e -x
 
 source ./build-env.sh
 
-tag="v8.1.0990"
+tag="v8.1.1017"
+patch_dir="$PWD/vim"
+patch_list=(
+"0001-Temporary-fix-for-man.vim.patch"
+)
 
 mkdir ${build_dir}
 pushd ${build_dir}
@@ -13,6 +17,9 @@ mkdir -p $PWD/vim
 pushd $PWD/vim
 git clone https://github.com/vim/vim.git --no-checkout --depth 1 --single-branch -b ${tag} $PWD
 git checkout refs/tags/${tag} -b build
+for p in ${patch_list}; do
+	git am ${patch_dir}/$p
+done
 ./configure --target=x86_64-linux-gnu --host=x86_64-linux-gnu --build=x86_64-linux-gnu --prefix=${install_prefix} --with-features=huge --enable-multibyte --enable-rubyinterp --enable-tclinterp --enable-luainterp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-perlinterp --enable-luainterp --enable-gui=no --enable-cscope --enable-hangulinput
 make -j ${jobs}
 make -j ${jobs} install
