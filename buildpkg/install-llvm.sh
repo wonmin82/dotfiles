17841e1c3b8e302b955e6c20338b6266a3813668
Wonmin Jung <wonmin82@gmail.com>
2019-03-26 22:19:34 +0900

    Update llvm installation script

--- buildpkg/install-llvm.sh
+++ buildpkg/install-llvm.sh
@@ -2,28 +2,28 @@
 
 function add_include_path()
 {
-    include=$1
-    shift
-    search_paths=$@
-    new_includes=${default_includes}
+	include=$1
+	shift
+	search_paths=$@
+	new_includes=${default_includes}
 
-    for p in ${search_paths}; do
-        test -d ${p} || continue
+	for p in ${search_paths}; do
+		test -d ${p} || continue
 
-        path=`find "${p}" | grep "${include}$" | awk '{print length, $0;}' | sort -n | head -1 | awk '{printf("%s", $2)}'`
+		path=`find "${p}" | grep "${include}$" | awk '{print length, $0;}' | sort -n | head -1 | awk '{printf("%s", $2)}'`
 
-        if [ -n "${path}" ]; then
-            path=`echo -n ${path} | sed "s#/${include}##g"`
-            if [ -n "${new_includes}" ] && ! $(echo "${new_includes}" | tr ":" "\n" | grep -qx "${path}"); then
-                new_includes="${new_includes}:${path}"
-            else
-                new_includes="${new_includes}"
-            fi
-            break
-        fi
-    done
+		if [ -n "${path}" ]; then
+			path=`echo -n ${path} | sed "s#/${include}##g"`
+			if [ -n "${new_includes}" ] && ! $(echo "${new_includes}" | tr ":" "\n" | grep -qx "${path}"); then
+				new_includes="${new_includes}:${path}"
+			else
+				new_includes="${new_includes}"
+			fi
+			break
+		fi
+	done
 
-    echo -n ${new_includes}
+	echo -n ${new_includes}
 }
 
 set -e -x
@@ -32,12 +32,13 @@ source ./build-env.sh
 
 llvm_tag="llvmorg-8.0.0"
 
-triple=$(make -v 2>&1 |                                                 \
+triple_gcc=$(gcc -v 2>&1 | grep "^Target:" | cut -d ' ' -f 2)
+triple_make=$(make -v 2>&1 |                                            \
 	grep -E "^This program built for |^Built for " |                    \
 	sed -e "s/This program built for //" -e "s/^Built for //")
 
 default_includes=$(add_include_path features.h /usr/include)
-default_includes=$(add_include_path sys/cdefs.h /usr/include/${triple} /usr/include)
+default_includes=$(add_include_path sys/cdefs.h /usr/include/${triple_gcc} /usr/include)
 
 llvm_srcdir="${build_dir}/llvm-project"
 venv_dir="${build_dir}/.venv"
@@ -50,14 +51,16 @@ common_cmake="-G \"Unix Makefiles\"
 -DCMAKE_BUILD_TYPE=\"Release\"
 -DLLVM_PARALLEL_COMPILE_JOBS=${jobs}
 -DLLVM_PARALLEL_LINK_JOBS=${jobs}
--DLLVM_DEFAULT_TARGET_TRIPLE=\"${triple}\"
--DLLVM_HOST_TRIPLE=\"${triple}\"
+-DLLVM_DEFAULT_TARGET_TRIPLE=\"${triple_make}\"
+-DLLVM_HOST_TRIPLE=\"${triple_make}\"
 -DC_INCLUDE_DIRS=\"${default_includes}\"
 -DLLVM_ENABLE_PIC=\"on\"
 -DLLVM_ENABLE_RTTI=\"on\"
+-DLLVM_ENABLE_WARNINGS=\"off\"
 -DLLVM_ENABLE_ASSERTIONS=\"off\"
 -DBUILD_SHARED_LIBS=\"on\"
--DPYTHON_EXECUTABLE:FILEPATH=\"${venv_dir}/bin/python\""
+-DPYTHON_EXECUTABLE:FILEPATH=\"${venv_dir}/bin/python\"
+"
 
 # stage 0
 stage0_host_gcc_dir="$(which gcc | sed -e 's/\/bin\/gcc$//')"
@@ -77,7 +80,8 @@ stage0_cmake="${common_cmake}
 -DLLVM_BUILD_TOOLS=\"off\"
 -DLLVM_BUILD_DOCS=\"off\"
 -DLLVM_ENABLE_DOXYGEN=\"off\"
--DLLVM_ENABLE_SPHINX=\"off\""
+-DLLVM_ENABLE_SPHINX=\"off\"
+"
 
 # stage 1
 stage1_cc="${install_prefix}/bin/clang"
@@ -96,14 +100,15 @@ stage1_cmake="${common_cmake}
 -DLLVM_BUILD_TOOLS=\"off\"
 -DLLVM_BUILD_DOCS=\"off\"
 -DLLVM_ENABLE_DOXYGEN=\"off\"
--DLLVM_ENABLE_SPHINX=\"off\""
+-DLLVM_ENABLE_SPHINX=\"off\"
+"
 
 # stage 2
 stage2_cc="${install_prefix}/bin/clang"
 stage2_cxx="${install_prefix}/bin/clang++"
-stage2_cflags="${common_cflags}"
-stage2_cxxflags="${common_cxxflags}"
-stage2_ldflags="-lstdc++ ${common_ldflags}"
+stage2_cflags="${common_cflags} -stdlib=libc++"
+stage2_cxxflags="${common_cxxflags} -stdlib=libc++"
+stage2_ldflags="${common_ldflags} -stdlib=libc++"
 stage2_cmake="${common_cmake}
 -DCMAKE_C_COMPILER=\"${stage2_cc}\"
 -DCMAKE_CXX_COMPILER=\"${stage2_cxx}\"
@@ -112,12 +117,14 @@ stage2_cmake="${common_cmake}
 -DCMAKE_SHARED_LINKER_FLAGS=\"${stage2_ldflags}\"
 -DLLVM_TARGETS_TO_BUILD=\"all\"
 -DLLVM_ENABLE_PROJECTS=\"all\"
+-DCLANG_DEFAULT_CXX_STDLIB=\"libc++\"
 -DLLVM_BUILD_TOOLS=\"on\"
 -DLLVM_BUILD_DOCS=\"on\"
 -DLLVM_ENABLE_DOXYGEN=\"off\"
 -DLLVM_ENABLE_SPHINX=\"on\"
 -DSPHINX_OUTPUT_MAN=\"on\"
--DSPHINX_WARNINGS_AS_ERRORS=\"off\""
+-DSPHINX_WARNINGS_AS_ERRORS=\"off\"
+"
 
 # if ! command -v sphinx-build 2> /dev/null; then
 #     stage2_cmake=$(sed -e "s/-DLLVM_ENABLE_SPHINX=\"on\"/-DLLVM_ENABLE_SPHINX=\"off\"/g" <<< ${stage2_cmake})
@@ -149,8 +156,8 @@ mkdir -p ${build_dir}/stage0
 pushd ${build_dir}/stage0
 
 eval cmake ${stage0_cmake} ${llvm_srcdir}/llvm
-make -j ${jobs}
-make -j ${jobs} install
+cmake --build $PWD -- -j ${jobs}
+cmake --build $PWD --target install -- -j ${jobs}
 
 popd
 
@@ -159,8 +166,8 @@ mkdir -p ${build_dir}/stage1
 pushd ${build_dir}/stage1
 
 eval cmake ${stage1_cmake} ${llvm_srcdir}/llvm
-make -j ${jobs}
-make -j ${jobs} install
+cmake --build $PWD -- -j ${jobs}
+cmake --build $PWD --target install -- -j ${jobs}
 
 popd
 
@@ -169,8 +176,8 @@ mkdir -p ${build_dir}/stage2
 pushd ${build_dir}/stage2
 
 eval cmake ${stage2_cmake} ${llvm_srcdir}/llvm
-make -j ${jobs}
-make -j ${jobs} install
+cmake --build $PWD -- -j ${jobs}
+cmake --build $PWD --target install -- -j ${jobs}
 
 popd
 
