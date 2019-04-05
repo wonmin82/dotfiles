59306104a7d54fe982c0fc2a733ce6c4c55b2a12
Wonmin Jung <wonmin82@gmail.com>
2019-04-05 23:24:13 +0900

    Fix ubuntu config

--- install-ubuntu-config.sh
+++ install-ubuntu-config.sh
@@ -13,9 +13,10 @@ cp -f -v $PWD/ubuntu/fonts.conf $HOME/.config/fontconfig/
 
 sudo mkdir -p -v /root/.config/pip
 sudo cp -f -v $PWD/ubuntu/pip.conf /root/.config/pip/
+sudo chown -R -v root:root /root/.config/pip/pip.conf
 sudo mkdir -p -v /root/.config/fontconfig
 sudo cp -f -v $PWD/ubuntu/fonts.conf /root/.config/fontconfig/
-sudo chown -v root:root /root/.config/fontconfig/
+sudo chown -R -v root:root /root/.config/fontconfig/fonts.conf
 
 sudo mkdir -p -v /etc/fonts
 sudo cp -f -v $PWD/ubuntu/local.conf /etc/fonts/
@@ -24,7 +25,9 @@ sudo chown -v root:root /etc/fonts/local.conf
 sudo mkdir -p -v /etc/apt/preferences.d
 sudo cp -f -v $PWD/ubuntu/preferences.d/nodejs /etc/apt/preferences.d/
 sudo cp -f -v $PWD/ubuntu/preferences.d/runit /etc/apt/preferences.d/
-sudo chown -v root:root /etc/apt/preferences
+sudo chown -v root:root                     \
+		 /etc/apt/preferences.d/nodejs      \
+		 /etc/apt/preferences.d/runit
 
 sudo mkdir -p -v /etc/apt/apt.conf.d
 sudo cp -f -v $PWD/ubuntu/99dpkg-options /etc/apt/apt.conf.d/
