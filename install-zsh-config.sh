5c6312fb5f23883bacc821158d20f5b9a2fbff6d
Wonmin Jung <wonmin82@gmail.com>
2019-04-11 22:26:54 +0900

    Add ubuntu zkbd configs

--- install-zsh-config.sh
+++ install-zsh-config.sh
@@ -4,10 +4,12 @@ cp -f -v ./zsh/.zshenv ~/.zshenv
 mkdir -p -v ~/.zsh
 cp -f -v ./zsh/.zshrc ~/.zsh/.zshrc
 mkdir -p -v ~/.zsh/.zkbd
+cp -f -v ./zsh/.zkbd/xterm-256color-ubuntu-linux-gnu ~/.zsh/.zkbd/
+cp -f -v ./zsh/.zkbd/screen-256color-ubuntu-linux-gnu ~/.zsh/.zkbd/
+ln -s -f ~/.zsh/.zkbd/xterm-256color-ubuntu-linux-gnu ~/.zsh/.zkbd/xterm-256color-unknown-linux-gnu
+ln -s -f ~/.zsh/.zkbd/screen-256color-ubuntu-linux-gnu ~/.zsh/.zkbd/screen-256color-unknown-linux-gnu
 cp -f -v ./zsh/.zkbd/xterm-256color-pc-linux-gnu ~/.zsh/.zkbd/
 cp -f -v ./zsh/.zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/
-ln -s -f ~/.zsh/.zkbd/xterm-256color-pc-linux-gnu ~/.zsh/.zkbd/xterm-256color-unknown-linux-gnu
-ln -s -f ~/.zsh/.zkbd/screen-256color-pc-linux-gnu ~/.zsh/.zkbd/screen-256color-unknown-linux-gnu
 cp -f -v ./zsh/.zkbd/xterm-256color-apple-darwin18.0 ~/.zsh/.zkbd/
 cp -f -v ./zsh/.zkbd/screen-256color-apple-darwin18.0 ~/.zsh/.zkbd/
 
