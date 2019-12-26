0ca94f78f0861bc09d7bd369a97e4896d571236b
Wonmin Jung <wonmin82@gmail.com>
2019-12-26 20:40:25 +0900

    Update package versions

--- buildpkg/install-cppcheck.sh
+++ buildpkg/install-cppcheck.sh
@@ -4,7 +4,7 @@ set -e -x
 
 source ./build-env.sh
 
-tag="1.89"
+tag="1.90"
 
 mkdir ${build_dir}
 pushd ${build_dir}
--- buildpkg/install-git.sh
+++ buildpkg/install-git.sh
@@ -4,7 +4,7 @@ set -e -x
 
 source ./build-env.sh
 
-version="2.23.0"
+version="2.24.1"
 
 mkdir ${build_dir}
 pushd ${build_dir}
