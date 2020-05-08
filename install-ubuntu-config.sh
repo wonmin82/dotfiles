5bec33b66333d2b1946f3a4ca8ccdcd82155680f
Wonmin Jung <wonmin82@gmail.com>
2020-05-08 19:42:26 +0900

    Configuration for PPA priorities

--- install-ubuntu-config.sh
+++ install-ubuntu-config.sh
@@ -41,12 +41,11 @@ sudo cp -f -v $PWD/ubuntu/local.conf /etc/fonts/
 sudo chown -v root:root /etc/fonts/local.conf
 
 sudo mkdir -p -v /etc/apt/preferences.d
-sudo cp -f -v $PWD/ubuntu/preferences.d/llvm /etc/apt/preferences.d/
-sudo cp -f -v $PWD/ubuntu/preferences.d/nodejs /etc/apt/preferences.d/
+sudo cp -f -v $PWD/ubuntu/preferences.d/ppa /etc/apt/preferences.d/
 sudo cp -f -v $PWD/ubuntu/preferences.d/runit /etc/apt/preferences.d/
 sudo cp -f -v $PWD/ubuntu/preferences.d/docker /etc/apt/preferences.d/
 sudo chown -v root:root \
-	/etc/apt/preferences.d/nodejs \
+	/etc/apt/preferences.d/ppa \
 	/etc/apt/preferences.d/runit \
 	/etc/apt/preferences.d/docker
 
