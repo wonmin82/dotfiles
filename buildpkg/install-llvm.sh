f0442c197a7b7b0b66126a2173728f52ae04deeb
Wonmin Jung <wonmin82@gmail.com>
2020-10-13 18:27:48 +0900

    Disable libc in llvm build due to error

--- buildpkg/install-llvm.sh
+++ buildpkg/install-llvm.sh
@@ -121,7 +121,7 @@ stage2_ldflags="${common_ldflags} -stdlib=libc++ -lc++abi"
 # stage2_projects="all"
 # Full list for llvm 11.0.0
 # stage2_projects="clang;clang-tools-extra;compiler-rt;debuginfo-tests;libc;libclc;libcxx;libcxxabi;libunwind;lld;lldb;llgo;mlir;openmp;parallel-libs;polly;pstl"
-stage2_projects="clang;clang-tools-extra;compiler-rt;debuginfo-tests;libc;libclc;libcxx;libcxxabi;libunwind;lld;lldb;openmp;parallel-libs;polly;pstl"
+stage2_projects="clang;clang-tools-extra;compiler-rt;debuginfo-tests;libclc;libcxx;libcxxabi;libunwind;lld;lldb;openmp;parallel-libs;polly;pstl"
 stage2_cmake="${common_cmake}
 -DCMAKE_C_COMPILER=\"${stage2_cc}\"
 -DCMAKE_CXX_COMPILER=\"${stage2_cxx}\"
