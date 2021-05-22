f1ad488825a49f50fdb18bd13ed56f23d1ae3250
Wonmin Jung <wonmin82@gmail.com>
2021-05-22 17:31:00 +0900

    Add go packages required by vim-go

--- buildpkg/install-gopkgs.sh
+++ buildpkg/install-gopkgs.sh
@@ -4,11 +4,40 @@ set -e -x
 
 source ./build-env.sh
 
-list_install_pkgs=(
+list_pkgs_go=(
+	#+++ list from vim-go
+	"github.com/klauspost/asmfmt/cmd/asmfmt@latest"
+	"github.com/go-delve/delve/cmd/dlv@latest"
+	"github.com/kisielk/errcheck@latest"
+	"github.com/davidrjenni/reftools/cmd/fillstruct@master"
+	"github.com/rogpeppe/godef@latest"
+	"golang.org/x/tools/cmd/goimports@master"
+	"golang.org/x/lint/golint@master"
 	"golang.org/x/tools/gopls@latest"
+	"github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
+	"honnef.co/go/tools/cmd/staticcheck@latest"
+	"github.com/fatih/gomodifytags@latest"
+	"golang.org/x/tools/cmd/gorename@master"
+	"github.com/jstemmer/gotags@master"
+	"golang.org/x/tools/cmd/guru@master"
+	"github.com/josharian/impl@master"
+	"honnef.co/go/tools/cmd/keyify@master"
+	"github.com/fatih/motion@latest"
+	"github.com/koron/iferr@master"
+	#--- list from vim-go
+	"golang.org/x/tools/cmd/godoc"
+	"github.com/sqs/goreturns"
+	"golang.org/x/tools/cmd/gotype"
+)
+
+list_pkgs_go111=(
 	"mvdan.cc/sh/v3/cmd/shfmt"
 )
 
-for pkg in "${list_install_pkgs[@]}"; do
-	GO111MODULE=on go get ${pkg}
+for pkg in "${list_pkgs_go[@]}"; do
+	go get -u -v ${pkg}
+done
+
+for pkg in "${list_pkgs_go111[@]}"; do
+	GO111MODULE=on go get -u -v ${pkg}
 done
